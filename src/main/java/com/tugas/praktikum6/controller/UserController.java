79351822e32afeb26c8c65453926ae3b258cb212
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-04-13 13:47:44 +0700

    menambahkan string untuk memanggil loginPage

--- src/main/java/com/tugas/praktikum6/controller/UserController.java
+++ src/main/java/com/tugas/praktikum6/controller/UserController.java
@@ -3,6 +3,7 @@ package com.tugas.praktikum6.controller;
 
 import com.tugas.praktikum6.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,5 +11,11 @@ import java.util.List;
 @Controller
 public class UserController {
     private List<User> userList = new ArrayList<>();
+
+    @GetMapping("/")
+    public String loginPage(){
+        return "login";
+    }
+
 
 }

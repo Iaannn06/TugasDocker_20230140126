9e270a791552fdf0559cd367105e462eb15f7275
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-04-13 13:51:18 +0700

    menambahkan string untuk mengecek login admin

--- src/main/java/com/tugas/praktikum6/controller/UserController.java
+++ src/main/java/com/tugas/praktikum6/controller/UserController.java
@@ -4,7 +4,9 @@ package com.tugas.praktikum6.controller;
 import com.tugas.praktikum6.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +16,15 @@ public class UserController {
 
     @GetMapping("/")
     public String loginPage(){
+        return "login";
+    }
+
+    @PostMapping("/Login")
+    public String login(@RequestParam String username, @RequestParam String password, Model model) {
+        if ("admin".equals(username) && "20230140126".equals(password)) {
+            return "redirect:/home";
+        }
+        model.addAttribute("error", "Username atau Password salah!");
         return "login";
     }
 

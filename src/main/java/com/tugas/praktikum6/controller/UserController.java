cfe53041a856e85aa46a42ecc7daf4e65a4f917c
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-04-13 13:46:23 +0700

    menambahkan list untuk user

---
+++ src/main/java/com/tugas/praktikum6/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.tugas.praktikum6.controller;
+
+
+import com.tugas.praktikum6.model.User;
+import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+    private List<User> userList = new ArrayList<>();
+
+}

ca5d6952b96ff10431898c207ed7ff8dcc7bcae4
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-04-27 14:13:21 +0700

    menambahkan user.java

--- src/main/java/com/tugas/praktikum6/model/User.java
+++ src/main/java/com/tugas/praktikum6/model/User.java
@@ -1,9 +1,21 @@
 package com.tugas.praktikum6.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Entity
 @Data
+@Builder
+@Table (name = "NIM_users")
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class User {
     private String nama;
     private String nim;
